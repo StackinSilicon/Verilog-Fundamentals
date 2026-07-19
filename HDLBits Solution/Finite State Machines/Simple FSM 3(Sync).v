2d1ad9ad95ae8d2893cad213de0af4b43b082d24
StackinSilicon <ameypathak04@gmail.com>
2026-07-19 23:11:34 +0530

    Add files via upload

---
+++ HDLBits Solution/Finite State Machines/Simple FSM 3(Sync).v
@@ -0,0 +1,28 @@
+module top_module(
+    input clk,
+    input in,
+    input reset,
+    output out);
+    
+    parameter A=2'b00,B=2'b01,C=2'b10,D=2'b11;
+
+    reg [1:0] cur,nxt;
+    
+    always @(posedge clk) begin
+        if(reset)
+            cur <= A;
+        else cur <= nxt; 
+    end 
+    always @(*) begin  
+        case(cur)
+            A: nxt = in? B:A;
+            B: nxt = in? B:C;
+            C: nxt = in? D:A;
+            D: nxt = in? B:C;
+            default: nxt = A;
+        endcase
+    end
+
+    assign out = (cur == D);
+
+endmodule
